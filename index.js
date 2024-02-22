e06fd9b58a7354ab903b74e325f00dcd1fb03078
Michelle Pettersson <156910491+Annamichelles@users.noreply.github.com>
2024-02-22 18:26:15 +0100

    wip

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-document.getElementById("myCanvas");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const centerX = canvas.width / 2; // X-koordinaten för cirkelns centrum
+const centerY = canvas.height / 2; // Y-koordinaten för cirkelns centrum
+const radius = 50; // Radien för cirkeln
+
+ctx.beginPath(); // Börja en ny stig för att rita
+ctx.arc(2, 2, 50, 0, 2 * 3.14); // Rita cirkeln
+ctx.fillStyle = 'blue'; // Ställ in fyllfärgen till blå
+ctx.fill('blue'); // Fyll cirkeln med den angivna färgen
+ctx.closePath(); // Avsluta stigen
+
+
+/*functions*/
 
 function bouncingballs(parameter1, parameter2, parameter3) {
-  
+  draw
 }
 
 bouncingballs ()
