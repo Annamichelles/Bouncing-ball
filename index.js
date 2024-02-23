fb1d5636219bed8f48bcead1e37c80af5db41919
Michelle Pettersson <156910491+Annamichelles@users.noreply.github.com>
2024-02-23 13:15:37 +0100

    circle wip

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+ctx.beginPath(); 
+ctx.moveTo(100,100);
+ctx.moveTo(200,200);
+ctx.closePath(); 
+
+/*circle*/
+
 const centerX = canvas.width / 2; // X-koordinaten för cirkelns centrum
 const centerY = canvas.height / 2; // Y-koordinaten för cirkelns centrum
 const radius = 50; // Radien för cirkeln
-
-ctx.beginPath(); // Börja en ny stig för att rita
-ctx.arc(2, 2, 50, 0, 2 * 3.14); // Rita cirkeln
-ctx.fillStyle = 'blue'; // Ställ in fyllfärgen till blå
 ctx.fill('blue'); // Fyll cirkeln med den angivna färgen
-ctx.closePath(); // Avsluta stigen
-
+ctx.arc(2, 2, 50, 0, 2 * Math.PI); // Rita cirkeln
+ctx.fillStyle = 'blue'; // Ställ in fyllfärgen till blå
 
 /*functions*/
 
