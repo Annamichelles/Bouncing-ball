ddadd3536b5706eb5f2eb5b0d4407a8ba952cbe3
Michelle Pettersson <156910491+Annamichelles@users.noreply.github.com>
2024-02-23 13:52:46 +0100

    ball done

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+/*canvas-background*/
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
 
-ctx.beginPath(); 
-ctx.moveTo(100,100);
-ctx.moveTo(200,200);
-ctx.closePath(); 
 
-/*circle*/
+    /*circle*/
 
-const centerX = canvas.width / 2; // X-koordinaten för cirkelns centrum
-const centerY = canvas.height / 2; // Y-koordinaten för cirkelns centrum
-const radius = 50; // Radien för cirkeln
-ctx.fill('blue'); // Fyll cirkeln med den angivna färgen
-ctx.arc(2, 2, 50, 0, 2 * Math.PI); // Rita cirkeln
-ctx.fillStyle = 'blue'; // Ställ in fyllfärgen till blå
+    const centerX = canvas.width / 2; 
+    const centerY = canvas.height / 2; 
+    const radius = 50; 
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "pink"; 
+    ctx.fill ();
+    ctx.beginPath(); 
+    ctx.moveTo(100,100);
+    ctx.lineTo(200,200);
+    ctx.closePath(); 
 
-/*functions*/
+    /*functions*/
 
-function bouncingballs(parameter1, parameter2, parameter3) {
-  draw
-}
+   
 
-bouncingballs ()
+    });
